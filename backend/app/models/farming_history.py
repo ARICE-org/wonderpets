f7449658e97aeff0aeadb620a0ca5c56e20d0711
Markrodriguez1105 <markanthonyrodriguez59@gmail.com>
2025-09-09 14:27:56 +0800

    feat: update farmer_id to UUID in models and seeders

--- backend/app/models/farming_history.py
+++ backend/app/models/farming_history.py
@@ -1,14 +1,11 @@
 from sqlalchemy import Column, String, Date, Numeric, ForeignKey
-from .farmer import Farmer
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.postgresql import UUID
 from app.db import Base
-
-# Base = declarative_base()
 
 class FarmingHistory(Base):
     __tablename__ = "farming_history"
     history_id = Column(String(20), primary_key=True)
-    farmer_id = Column(String(20), ForeignKey("farmer.farmer_id"), nullable=False)
+    farmer_id = Column(UUID(as_uuid=True), ForeignKey("farmer.farmer_id"), nullable=False)
     history_date = Column(Date)
     history_title = Column(String(100))
     yields = Column(Numeric)
